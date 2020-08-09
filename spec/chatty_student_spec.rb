e48323696bb71a419ce92b4b93e101c3d729f01e
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-08-09 18:16:09 +0000

    Done.

--- spec/chatty_student_spec.rb
+++ spec/chatty_student_spec.rb
@@ -4,7 +4,7 @@ describe "ChattyStudent" do
   
   let!(:chatty_student) { ChattyStudent.new }
   
-  end
+  
 
   describe "#raise_hand" do 
     it "returns the phrase 'Pick me!' ten times." do 
@@ -15,4 +15,5 @@ describe "ChattyStudent" do
     it "returns a greeting phrase plus a long chatty phrase" do  
       expect{chatty_student.hello}.to output("Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n").to_stdout
     end
+  end 
 end
